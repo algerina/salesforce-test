c19b56ee23a5a21e2ef60e43cd77780bab93311f
Amel Khiri <53347966+algerina@users.noreply.github.com>
2023-05-20 17:39:43 +0100

    Implement features

--- src/components/CandidatureList.js
+++ src/components/CandidatureList.js
@@ -1,31 +1,37 @@
-import React { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import Candidature from './Candidature';
 
 const CandidatureList = () => {
   const [candidatures, setCandidatures] = useState([]);
 
   useEffect(() => {
+    // eslint-disable-next-line no-use-before-define
     fetchCandidatures();
   }, []);
 
   const fetchCandidatures = async () => {
-    const { data } = await axios.get('/api/candidatures');
-    setCandidatures(data);
-    };
-    return (
-      <div>
-        <h1>Candidatures</h1>
-        <ul>
-          {candidatures.map((candidature) => (
-            <li key={candidature._id}>
-              <h2>{candidature.First_Name_ _c}</h2>
-              <p>{candidature.Last_Name_ _ c}</p>
-              <p>{candidature.Year_Of_Experience}</p>
-              </li>
-          ))}
-               </ul>
-               </div>
-               );
-               }
+    try {
+      const endpoint = 'https://your_instance.salesforce.com/services/data/v55.0/query/?q=SELECT+First_Name__c,Last_Name__c,Year__c,Year_Of_Experience__c+FROM+Candidature__c';
+      const response = await axios.get(endpoint, {
+        headers: { Authorization: 'Bearer your_access_token' },
+      });
+      setCandidatures(response.data.records);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
-               export default CandidatureList;
+  return (
+    <div>
+      <h2>Liste des candidatures</h2>
+      <ul>
+        {candidatures.map((candidature) => (
+          <Candidature key={candidature.id} candidature={candidature} />
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default CandidatureList;
